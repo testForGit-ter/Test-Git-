a6485b50fedcdd45a29d1dd5538444c57ec5bdb8
testForGit-ter <nam787831@gmail.com>
2024-11-05 19:46:57 +0700

    Create test.java

---
+++ test.java
@@ -0,0 +1,84 @@
+
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * GiangVien
+ */
+class GiangVien {
+    static int ma = 1;
+    String ten, hesokk, MA;
+    double hesoLuong;
+    int hour;
+    int luong;
+
+    public GiangVien(String ten, double hesoLuong, String hesoKK, int hour)
+    {
+        this.ten = ten;
+        this.hesoLuong = hesoLuong;
+        this.hesokk = hesoKK;
+        this.hour = hour;
+        MA = "PM" + String.format("%03d", ma++);
+    }
+
+    public long GetLuong()
+    {
+       double hs;
+       if(hesokk.equals("A")){
+        hs = 1.5;
+       } else if(hesokk.equals("B")){
+        hs = 1.2;
+       }else{
+        hs = 1;
+       }
+
+       double luo = hesoLuong * hs * 3000;
+       if(hour <= 100){
+        luo *= 0.85;
+       }else if(hour <= 350){
+        luo *= 0.9;
+       }else if(hour <= 499){
+        luo *= 0.95;
+       }
+
+       return Math.round(luo);
+
+
+    }
+
+    public String GetLowName()
+    {
+        return this.ten.toLowerCase();
+    }
+
+    @Override
+    public String toString() {
+        return this.MA + " " + this.ten + " " + this.hesoLuong + " " 
+            + this.hesokk +" " + this.hour + " " + this.GetLuong();
+    }
+
+
+    
+}
+
+public class b3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        scanner.nextLine();
+        List<GiangVien> giangViens = new ArrayList<>();
+        while (t-- > 0) {
+            GiangVien giangVien = new GiangVien(scanner.nextLine(), Double.parseDouble(scanner.nextLine()), scanner.nextLine(), Integer.parseInt(scanner.nextLine()));
+            giangViens.add(giangVien);
+        }
+        String check = scanner.nextLine();
+        for (GiangVien giangVien : giangViens) {
+            if(giangVien.GetLowName().contains(check))
+                System.out.println(giangVien);
+
+        }
+        scanner.close();
+    }
+
+}
